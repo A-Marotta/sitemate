ea23d1145cc968fa55cf95caf5f52818e3864cd0
Adrian <a.marotta97@gmail.com>
2024-06-10 16:18:15 +1000

    add create route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import Issue from './issue';
 
 class Server {
     private app: express.Application;
@@ -20,6 +21,21 @@ class Server {
         const router = express.Router();
         this.app.use('/api', router);
 
+        router.post('/create', this.handleCreate);
+    }
+
+    private handleCreate(req: Request, res: Response): void {
+        const { title, description } = req.body;
+
+        const newIssue: Issue = new Issue(Issue.generateId(), title, description)
+
+        console.log(newIssue);
+
+        res.status(201).send({
+            id: newIssue.id,
+            title: newIssue.title,
+            description: newIssue.description
+        });
     }
 
     public start(): void {
